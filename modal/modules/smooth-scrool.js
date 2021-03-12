29138715189c805070a962a5c33dfa192de08880
Debora <deboraro.61@gmail.com>
2021-03-12 16:57:12 -0300

    javascript separado em módulos

---
+++ modal/modules/smooth-scrool.js
@@ -0,0 +1,20 @@
+//scroll suave
+//clique no link e role a página
+
+export default function smoothScrool(){
+  const internalLinks = document.querySelectorAll(".js-internalLinks a[href^='#']");
+
+  internalLinks.forEach((item)=>{
+    item.addEventListener("click",(event)=>{
+      event.preventDefault();
+      let hrefItem = item.getAttribute("href");
+      activeScrool(hrefItem);
+    }
+    )
+  })
+
+  function activeScrool(item){
+    const itemId = document.querySelector(item);
+    itemId.scrollIntoView({block: "start", behavior: "smooth"});
+  }
+}
