e239c9e698bff58124b539812af6dd26ff723874
Debora <deboraro.61@gmail.com>
2021-03-13 17:44:48 -0300

    add o comportamento do modal

--- modal/script.js
+++ modal/script.js
@@ -1,7 +1,9 @@
 import initTabNav from './modules/init-tabnav.js';
 import initAccordion from './modules/init-accordion.js';
 import smoothScrool from './modules/smooth-scrool.js';
+import initModal from './modules/init-modal.js';
 
 initTabNav();
 initAccordion()
 smoothScrool();
+initModal();
