6abbd713e69c3eaaa652b699da658d8650d00571
Debora <deboraro.61@gmail.com>
2021-03-16 17:29:06 -0300

    tooltip implementado

--- modal/modules/init-tooltip.js
+++ modal/modules/init-tooltip.js
@@ -1,17 +1,39 @@
-export default initToolTip()
+export default function initToolTip()
 {
-
-}
-
-
 const tooltips = document.querySelectorAll('[data-tooltip]');
 
 
 tooltips.forEach((item)=>{
-  item.addEventListener("mousemove", onMouseMove);
+  item.addEventListener("mouseover", onMouseOver);
 })
 
-function onMouseMove(event){
-  console.log(event);
+function onMouseOver(event){
+  const toolTipBox = criarToolTipBox(this);
+  toolTipBox.style.top = event.pageY + 'px';
+  toolTipBox.style.left = event.pageX + 'px';
+
+
+  onMouseLeave.toolTipBox = toolTipBox;
+  this.addEventListener("mouseleave", onMouseLeave);
+
 }
+
+const onMouseLeave ={
+    toolTipBox:"",
+    handleEvent() {this.toolTipBox.remove()},
+    
+}
+
+function criarToolTipBox(elemento){
+  const toolTipBox = document.createElement('div');
+  toolTipBox.classList.add("tooltip");
+  const texto = elemento.getAttribute("aria-label");
+  toolTipBox.innerText = texto;
+  document.body.appendChild(toolTipBox);
+  return toolTipBox;
+}
+
+}
+
+
 
