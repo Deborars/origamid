6ebcf65c8380abb38ad8becf57f7f0135fefca57
Debora Silva <deboraro.61@gmail.com>
2023-07-09 20:29:51 -0300

    conclusão do modulo 03 DOM para Iniciantes

--- animais-fantasticos/script.js
+++ animais-fantasticos/script.js
@@ -1,3 +1,6 @@
+
+
+
 function initTabNav(){
 const tabMenu = document.querySelectorAll(".js-tabmenu li");
 const tabContent = document.querySelectorAll(".js-tabcontent article");
@@ -66,3 +69,31 @@ function SmoothScrool(){
 }
 
 SmoothScrool();
+
+
+function initAnimacaoScroll(){
+
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if(sections.length){
+  const window60per = window.innerHeight * 0.6;
+
+    function animaScroll(){
+      sections.forEach((section)=>{
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - window60per) < 0;
+        if(isSectionVisible){
+          console.log('animar');
+          section.classList.add('ativo');
+        }
+      })
+    }
+
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+
+  }
+}
+
+initAnimacaoScroll();
