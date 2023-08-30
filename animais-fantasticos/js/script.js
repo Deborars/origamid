d217f1c1dc0931bd0b27eb8907fa082e901b66c8
Debora Silva <deboraro.61@gmail.com>
2023-08-29 23:02:07 -0300

    concluida aula sobre modal

--- animais-fantasticos/js/script.js
+++ animais-fantasticos/js/script.js
@@ -3,9 +3,11 @@ import SmoothScrool from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js'
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
 
 
 SmoothScrool();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal();
