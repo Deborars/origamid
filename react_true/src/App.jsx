ad989b68071209150ba8dec5c234ed7c629c12c4
Debora Silva <deboraro.61@gmail.com>
2023-09-02 21:00:36 -0300

    implementando desafio de componentes

---
+++ react_true/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./Header2.jsx";
+import Home from "./Home.jsx";
+
+
+const App = () =>{
+  const { pathname } = window.location;
+  let Component = '';
+  if(pathname == './produtos'){
+      Component = Produtos;
+  }else{
+      Component = Home;
+  }
+
+  return(
+    <>
+      <Header/>
+      <Component/>
+    </>
+  )
+}
+
+export default App;
