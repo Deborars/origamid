becc8253b9f939cf71b723bd498397cfe0dc6282
Debora Silva <deboraro.61@gmail.com>
2023-09-18 21:59:12 -0300

    exercicio checkbox2

--- react_form/src/App.jsx
+++ react_form/src/App.jsx
@@ -1,104 +1,45 @@
-import React from 'react'
+import React from "react";
 
-    const formFields = [
-      {
-        id:'nome',
-        label:'Nome',
-        type:'text'
-      },
-      {
-        id:'email',
-        label:'Email',
-        type:'email'
-      },
-            {
-        id:'senha',
-        label:'Senha',
-        type:'password'
-      },
-      {
-        id:'cep',
-        label:'Cep',
-        type:'text'
-      },
-      {
-        id:'rua',
-        label:'Rua',
-        type:'text'
-      },
-      {
-        id:'numero',
-        label:'Numero',
-        type:'text'
-      },
-      {
-        id:'bairro',
-        label:'Bairro',
-        type:'text'
-      },
-      {
-        id:'cidade',
-        label:'Cidade',
-        type:'text'
-      },
-      {
-        id:'estado',
-        label:'Estado',
-        type:'text'
-      }
+const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
 
-    ]
+const App = () => {
+  const [cores, setCores] = React.useState([]);
 
-function App() {
-
-
-    const [form, setForm] = React.useState({
-      nome: '',
-      email: '',
-      senha: '',
-      cep: '',
-      rua: '',
-      numero: '',
-      bairro:'',
-      cidade:'',
-      estado:''
-    })
-
-    const [response, setResponse] = React.useState(null);
-
-    function handleSubmit (event){
-      event.preventDefault();
-      fetch('https://ranekapi.origamid.dev/json/api/usuario', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        // form é o objeto com os dados do formulário
-        body: JSON.stringify(form),
-      }).then((response)=>{
-          setResponse(response);
-      });
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
     }
+  }
 
-    function handleChange ({target}){
-      const {id, value} = target;
-      setForm({...form, [id]:value})
-    }
+  function handleChecked(cor) {
+    return cores.includes(cor);
+  }
 
   return (
-      <form onSubmit={handleSubmit} >
-      {formFields.map(({ id, label, type}) => (
-            <div key={id}>
-              <label htmlFor={id}>{label}</label>
-              <input type={type} id={id} value={form[id]} onChange={handleChange}/>
-            </div>
-          ))}
-        <button>Enviar</button>
-        {response && response.ok && <p>Dados enviado com sucesso!</p>}
-      </form>
-  )
-}
+    <form>
+      {coresArray.map((cor)=>(
+        
+        <label>
+          <input
+          type="checkbox"
+          value={cor}
+          checked={handleChecked(cor)}
+          onChange={handleChange}
+          />
+          {cor}
+        </label>
+      ))}
 
-export default App
 
+      <ul>
+        {cores.map((cor) => (
+          <li key={cor}>{cor}</li>
+        ))}
+      </ul>
+    </form>
+  );
+};
 
+export default App;
